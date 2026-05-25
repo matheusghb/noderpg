96d78a027ff569235f3449980db8bbc5ae7d5554
matheusghb <mistermquack124@gmail.com>
2026-05-24 21:44:59 -0300

    flex da interface adicionada, aprender a adicionar dinamicamente

--- main.js
+++ main.js
@@ -1,14 +1,65 @@
-const { QMainWindow, QLabel } = require("@nodegui/nodegui")
+const { QMainWindow, QLabel, QWidget, FlexLayout } = require("@nodegui/nodegui")
+
+const flexview = new QWidget();
+const layout = new FlexLayout()
+flexview.setLayout(layout)
+flexview.setObjectName("window")
+
+const leftdiv = new QWidget();
+leftdiv.setObjectName("ldiv")
+layout.addWidget(leftdiv)
+
+const rightdiv = new QWidget();
+rightdiv.setObjectName("rdiv")
+const rlayout = new FlexLayout()
+rightdiv.setLayout(rlayout)
+
+    const innerlabel = new QLabel();
+    innerlabel.setObjectName("label")
+    innerlabel.setText("Hello World")
+
+    rlayout.addWidget(innerlabel)
+
+layout.addWidget(rightdiv)
+
+flexview.setStyleSheet(`
+    
+    #window {
+        flex-direction: row;
+        flex: 1;
+        background-color: #120f20;
+    }
+
+    #ldiv {
+        flex: 1;
+        background-color: black;
+    }
+
+    #rdiv {
+        flex: 1.5;
+        margin: 7%;
+        border: 4px solid #badb00;
+        border-radius: 5px;
+    }
+
+    #label {
+
+        color: black;    
+        width: 100%;
+        height: 20%;
+        margin: 40%;
+        background-color: white;
+    
+    }
+
+
+`)
 
 const win = new QMainWindow();
-win.setMinimumSize(400,400)
+win.setMinimumSize(800,600)
 win.setMaximumHeight(3600);
 win.setMaximumWidth(2500);
-
-
-const displaynome = new QLabel(win); 
-displaynome.setText("seu nome é:");
-displaynome.setInlineStyle("color: black;");
+win.setCentralWidget(flexview);
 
 win.show();
 global.win = win;
